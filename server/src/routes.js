f21e0c8a0d0950f3653f4460f348f97053161041
ClodoaldoDantas <clodoaldodantas8@gmail.com>
2020-06-18 18:02:51 -0300

    criando as views

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const routes = express.Router();
 
+const recipes = require('./data/recipes');
+const featuredRecipes = recipes.slice(0, 6);
+
 routes.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { recipes: featuredRecipes });
+});
+
+routes.get('/about', (req, res) => {
+  res.render('about');
+});
+
+routes.get('/recipes', (req, res) => {
+  res.render('recipes', { recipes });
 });
 
 module.exports = routes;
