96b2211dec86d9395ef6203dcee1aa49933b0fd7
ClodoaldoDantas <clodoaldodantas8@gmail.com>
2020-06-30 22:17:41 -0300

    organizando as views

--- server/src/controllers/pages.js
+++ server/src/controllers/pages.js
@@ -2,20 +2,20 @@ const recipes = require("../../data.json");
 
 exports.index = (req, res) => {
   const filteredRecipes = recipes.slice(0, 6);
-  res.render("index", { recipes: filteredRecipes });
+  res.render("pages/index", { recipes: filteredRecipes });
 };
 
 exports.about = (req, res) => {
-  res.render("about");
+  res.render("pages/about");
 };
 
 exports.recipes = (req, res) => {
-  res.render("recipes", { recipes });
+  res.render("pages/recipes", { recipes });
 };
 
 exports.show = (req, res) => {
   const { index } = req.params;
   const recipe = recipes[index];
 
-  res.render("recipe", { recipe });
+  res.render("pages/recipe", { recipe });
 };
