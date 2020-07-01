f269e963dfd7729a8a0e463d359fa12946506fe4
ClodoaldoDantas <clodoaldodantas8@gmail.com>
2020-06-30 22:55:14 -0300

    melhorando css e iniciando rotas do admin

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,11 +3,15 @@ const routes = express.Router();
 
 // controllers
 const pages = require("./controllers/pages");
+const admin = require("./controllers/admin");
 
 // page routes
 routes.get("/", pages.index);
 routes.get("/about", pages.about);
 routes.get("/recipes", pages.recipes);
 routes.get("/recipes/:index", pages.show);
+
+// admin routes
+routes.get("/admin/recipes", admin.index);
 
 module.exports = routes;
