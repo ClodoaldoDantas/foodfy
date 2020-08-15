239210edd94c0455c758280d4d084cdd9f0f3e04
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2020-08-14 21:49:57 -0300

    refatorando funcao de validacao

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,17 +2,14 @@ exports.validateFields = (data) => {
   const keys = Object.keys(data);
   let valid = true;
 
-  keys.forEach((key) => {
-    const field = data[key];
-
+  for (key of keys) {
     if (Array.isArray(data[key])) {
-      field.forEach((value) => {
-        if (value === "") valid = false;
-      });
+      const empty = data[key].some((item) => item === "");
+      if (empty) valid = false;
     } else {
       if (data[key] === "") valid = false;
     }
-  });
+  }
 
   return valid;
 };
